33f90d630c4d445248e0d4aa186248e10634c510
Beata Johansson <78729326+beatajoh@users.noreply.github.com>
2023-06-16 11:37:41 +0200

    Add all files.
    To test run this command: python main.py

---
+++ a_star_shortest_path.py
@@ -0,0 +1,111 @@
+
+import json
+from collections import deque
+import heapq
+
+def all_parents_visited(atkgraph, neighbor, open_set, parent_nodes, visited):
+    print(neighbor)
+    print(parent_nodes)
+    if neighbor in parent_nodes:
+        print("AND NODE!!!",neighbor)
+        for parents in parent_nodes[neighbor]:
+            if visited[parents] == 0:
+                print("FALSE AND", neighbor)
+                return False 
+    return True
+    
+def reconstruct_path(came_from, current):
+    total_path = [current]
+    while current in came_from.keys():
+        current = came_from[current]
+        print("CURRENT",current)
+        total_path.insert(0,current)
+    return total_path
+
+def fill_dictionary(atkgraph, score):
+    scores = {}
+    for node in atkgraph:
+        scores[node['id']] = score
+    return scores
+
+def get_neighbor_nodes(atkgraph): 
+    dict = {}
+    for node in atkgraph:
+        dict[node['id']] = node['links']
+    return dict
+
+def get_parent_nodes_for_and_nodes(atkgraph): 
+    dict = {}
+    for node in atkgraph:
+        if node['type'] == 'and':
+            dict[node['id']] = node['parent_list']
+    return dict
+
+def get_costs_for_nodes(atkgraph):
+    dict = {}
+    for node in atkgraph:
+        print(node)
+        if not node['ttc']==None: # for the attacker node, the ttc is None
+            dict[node['id']]=node['ttc']['cost'][0]
+    return dict
+
+def a_star(atkgraph):
+
+    start_node = "A" # id attribute in json file
+    print(start_node)
+    target_node = "H"
+
+    open_set = []
+    heapq.heappush(open_set, (0, start_node))
+
+    visited = fill_dictionary(atkgraph,0)
+    visited[start_node]=1
+    came_from = fill_dictionary(atkgraph, "")
+
+    g_score = fill_dictionary(atkgraph, 10000) # map with default value of Infinity
+    g_score[start_node] = 0
+
+    h_score = fill_dictionary(atkgraph, 0)
+
+    # for node n, f_score[n] := g_score[n] + h(n). f_score[n] represents our current best guess as to
+    # how cheap a path could be from start to finish if it goes through n.
+    f_score = fill_dictionary(atkgraph, 0) # map with default value of Infinity
+    f_score[start_node] = h_score[start_node]
+
+    costs = get_costs_for_nodes(atkgraph)
+    neighbor_nodes = get_neighbor_nodes(atkgraph)
+    parent_nodes = get_parent_nodes_for_and_nodes(atkgraph)
+
+    current_node = start_node
+
+    while len(open_set) > 0:
+        print("OPEN SET        ",open_set)
+        # current_node = the node in openSet having the lowest f_score value
+        current_score, current_node = heapq.heappop(open_set)
+        visited[current_node]=1
+
+        if current_node == target_node:
+            print("Finished")
+            print(visited)
+            print(came_from)
+            return reconstruct_path(came_from, current_node)
+
+        current_neighbors = neighbor_nodes[current_node]
+        print(current_neighbors)
+        for neighbor in current_neighbors:
+            print(neighbor)
+            
+            tentative_g_score = g_score[current_node]+costs[neighbor] # d(current, neighbor)
+
+            if tentative_g_score < g_score[neighbor]:
+                if all_parents_visited(atkgraph, neighbor, open_set, parent_nodes, visited):
+                    came_from[neighbor] = current_node
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = tentative_g_score + 0 # h(neighbor) = 0
+                    if neighbor not in open_set:
+                        heapq.heappush(open_set, (f_score[neighbor], neighbor))
+                    
+    print("shortest path found ?")
+
+    return False # Open set is empty but goal was never reached
+   
---
+++ main.py
@@ -0,0 +1,80 @@
+import json
+from py2neo import Graph
+import a_star_shortest_path as a_star_shortest_path
+
+def main():
+    #with open("/Users/beatajohansson/Projects/mgg/tmp/atkgraph.json", 'r') as readfile:
+    file="small_graph_2.json"
+    with open(file, 'r') as readfile:
+        atkgraph = json.load(readfile)
+
+    atkgraph=get_parents_for_and_nodes(atkgraph)
+
+   
+    with open('ao_atkgraph.json', 'w', encoding='utf-8') as writefile:
+        json.dump(atkgraph, writefile, indent=4)
+    
+    path=a_star_shortest_path.a_star(atkgraph)
+
+    print("\n\n", path, "\n\n\n")
+    
+    
+'''
+Go through the attack graph .json file and get all parents to and nodes and add new "parent_list" attribute to the file.
+'''
+def get_parents_for_and_nodes(atkgraph):
+    n=0
+    id=""
+    id2=""
+    for i, node in enumerate(atkgraph):
+        id=node["id"]
+        parent_list=[]
+        if node["type"]=="and":
+            for node2 in atkgraph:
+                id2=node2["id"]
+                for link in node2["links"]:
+                    if link==id:
+                        parent_list.append(id2)
+            n+=1
+            atkgraph[i]["parent_list"]=parent_list
+    return atkgraph
+                    
+
+def convert_model_to_mgg(uri="bolt://localhost:7687", 
+            username="neo4j",
+            password="mgg12345!",
+            dbname="neo4j",
+            delete=False
+    ) -> dict:
+        print("hello")
+        g = Graph(uri=uri, user=username, password=password, name=dbname) 
+
+        # Get all relationships
+        results = g.run('MATCH paths=(source {name: "Attacker:6417249322885591:firstSteps"})-[*]->(target {name: "Application:7219598629313512:networkAccess"}) return paths'
+    ).data()
+        print(results)
+
+        objects = {}
+        associations = []
+
+        for row in results:
+            rel1 = list(row['r1'].types())[0]
+            rel2 = list(row['r2'].types())[0]
+            nodea = dict(row['a'])
+            nodeb = dict(row['b'])
+            objects[nodea['scad_id']] = {your_key: nodea[your_key] for your_key in ['name','metaconcept','eid']}
+            objects[nodeb['scad_id']] = {your_key: nodeb[your_key] for your_key in ['name','metaconcept','eid']}
+            add_elem = True
+            for assoc in associations:
+                if set([nodea['scad_id'],nodeb['scad_id'],rel1,rel2]) == set(assoc.values()):
+                    add_elem = False
+            if add_elem:
+                associations.append({'id1': nodea['scad_id'], 'id2': nodeb['scad_id'], 'type1': rel1, 'type2': rel2})
+
+
+        newmodel = {'metadata': {'scadVersion': '1.0.0', 'langVersion': '0.3.0', 'langID': 'org.mal-lang.coreLang', 'malVersion': '0.1.0-SNAPSHOT', 'info': 'Created with securiCAD Professional 1.6'}, 'objects': objects, 'associations': associations}
+        print("\n\nEXIT\n\n")
+        return newmodel
+
+if __name__=='__main__':
+     main()
